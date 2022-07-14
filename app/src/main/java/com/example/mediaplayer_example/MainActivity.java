10e793faba27f4d0008385b4b29f41d5a15db038
KotrotsKon <kotrotskon@users.noreply.github.com>
2022-07-14 18:53:09 +0300

    add pause button

--- app/src/main/java/com/example/mediaplayer_example/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer_example/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn_play = findViewById(R.id.btn_play);
+        Button btn_pause = findViewById(R.id.btn_pause);
 
         MediaPlayer player = MediaPlayer.create(MainActivity.this, R.raw.sound);
 
@@ -22,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 player.start();
+            }
+        });
+
+        btn_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.pause();
             }
         });
     }
