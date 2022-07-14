cf0d03ae15b84b87bd6c098f53ef6fbb4b86519b
KotrotsKon <kotrotskon@users.noreply.github.com>
2022-07-14 18:50:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mediaplayer_example/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mediaplayer_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_play = findViewById(R.id.btn_play);
+
+        MediaPlayer player = MediaPlayer.create(MainActivity.this, R.raw.sound);
+
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.start();
+            }
+        });
+    }
+}
